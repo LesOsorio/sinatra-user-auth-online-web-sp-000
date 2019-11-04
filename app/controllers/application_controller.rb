81aae9767b995054fd376d089009fe7d96642b70
Leslie Osorio <lesosorio17@gmail.com>
2019-11-04 17:07:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,6 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
-      puts params
     end
     redirect '/sessions/login'
   end
